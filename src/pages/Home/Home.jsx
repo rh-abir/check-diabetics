ee511067b1bd60a5557930338802fd4796b6a2d6
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-12-02 01:42:14 +0600

    New branch created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h2 className="text-center">THis is Home</h2>
+      <h2 className="text-center">THis is Home Created by Yasin Arafat</h2>
     </div>
   );
 };
