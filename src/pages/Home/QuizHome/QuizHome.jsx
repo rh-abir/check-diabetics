9480bdc5c2b03b496afa320b39a9ea8ad89d21bb
rh-abir <rhabir.co@gmail.com>
2023-12-10 00:50:48 +0600

    solve problem

--- src/pages/Home/QuizHome/QuizHome.jsx
+++ src/pages/Home/QuizHome/QuizHome.jsx
@@ -2,14 +2,12 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const QuizHome = () => {
-  const [isTrue, setTrue] = useState(true);
+  const [inputValue, setInputValue] = useState("");
 
-  const inputValue = (e) => {
-    const vlaue = e.target.value;
-    console.log(vlaue);
-    if (vlaue) {
-      setTrue(false);
-    }
+  console.log(inputValue);
+
+  const handleClick = () => {
+    console.log("hello word");
   };
 
   return (
@@ -27,17 +25,19 @@ const QuizHome = () => {
           type="text"
           placeholder="Enter Your Name"
           required
-          onKeyUp={inputValue}
+          onKeyUp={(e) => setInputValue(e.target.value)}
         />
       </form>
       <div className="flex justify-center items-center pt-4">
         <Link
           className={`px-4 py-1 border-0 rounded-md text-lg text-slate-200   duration-100 mb-2  ${
-            isTrue ? `bg-blue-200 text-slate-700` : `bg-blue-600`
+            inputValue.length < 1 ? `bg-blue-200 text-slate-700` : `bg-blue-600`
           }`}
           to={"/gender"}
         >
-          <button disabled={isTrue}>Start Test</button>
+          <button onClick={handleClick} disabled={inputValue.length < 1}>
+            Start Test
+          </button>
         </Link>
       </div>
     </div>
